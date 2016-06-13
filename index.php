35b9581fcfb2e37aa7602e451ee60fb937c3e3a0
imkrimerman <i.m.krimerman@gmail.com>
2016-06-13 10:11:16 +0300

    added heroku

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ $images = json_decode(file_get_contents($dir . DIRECTORY_SEPARATOR . 'images.jso
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css"/>
     <link rel="stylesheet" href="node_modules/flexslider/flexslider.css"/>
     <link rel="stylesheet" href="css/style.css"/>
